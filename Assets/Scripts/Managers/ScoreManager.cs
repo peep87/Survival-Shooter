8232e481345900d2266083b1b1fd0a5169f1e628
peep87 <1KIESEWETTER@hdsb.ca>
2017-04-18 14:17:16 -0400

    Uploaded Game to GitHub

---
+++ Assets/Scripts/Managers/ScoreManager.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class ScoreManager : MonoBehaviour
+{
+    public static int score;
+
+    public EnemyHealth a;
+
+    public Text text;
+
+
+    void Awake ()
+    {
+        a = GameObject.FindGameObjectWithTag("Zombunny").GetComponent<EnemyHealth>();  
+        text = GetComponent <Text> ();
+        score = 0;
+    }
+
+
+    void Update ()
+    {
+
+        text.text = "Score: " + score;
+    }
+}
